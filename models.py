fbd8d7e021a302278f6312f855cf71c318346053
Allan Pichardo <allan.pichardo@gmail.com>
2023-01-16 23:56:58 -0800

    Fixed image generation method and added it to the cppn class

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import torch
 from collections import OrderedDict
 from torch import nn
+from torchvision.io import write_jpeg
+from torchvision import transforms
 # from train import generate_image
 
 
@@ -32,12 +34,36 @@ class CPPN(nn.Module):
     def forward(self, x):
         return self.network(x)
 
+    def generate_image(self, image_shape, output_path, latent_dim=3):
+        to_pil = transforms.ToPILImage()
+        to_tensor = transforms.ToTensor()
+
+        image = torch.zeros(image_shape)
+        for x in range(image_shape[1]):
+            for y in range(image_shape[2]):
+                X = torch.tensor([[float(x / image_shape[2]), float(y / image_shape[1])]])
+
+                if latent_dim > 0:
+                    z = torch.zeros((X.shape[0], latent_dim))
+                    X = torch.cat([X, z], 1)
+
+                out = self.forward(X)
+                image[0][y][x] = out[0][0]
+                if image_shape[0] > 1:
+                    image[1][y][x] = out[0][1]
+                    image[2][y][x] = out[0][2]
+                if image_shape[0] == 4:
+                    image[3][y][x] = out[0][3]
+
+        image = to_pil(image)
+        image = to_tensor(image)
+        image *= 255
+        image = image.to(torch.uint8)
+        write_jpeg(image, output_path)
+
 
 if __name__ == '__main__':
     model = CPPN(num_layers=9, num_nodes=32).to('cpu')
-    print(model)
-
-    for param in model.named_parameters():
-        print(param)
+    model.generate_image((3, 64, 64), 'test.jpg', latent_dim=1)
 
     # generate_image(model, (3, 64, 64), 'test.jpg', latent_dim=1)
