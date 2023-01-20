7717d74b35be682faeff4bca5e2aafd91c84ef87
Allan Pichardo <allan.pichardo@gmail.com>
2023-01-20 00:03:37 -0800

    Using smaller cppn network

--- train.sh
+++ train.sh
@@ -2,13 +2,13 @@
 
 lightweight_gan \
     --data /home/allan/portrait-generator/collages \
-    --name cppn-test-1 \
-    --batch-size 16 \
+    --name cppn-test-2 \
+    --batch-size 24 \
     --gradient-accumulate-every 4 \
     --num-train-steps 200000 \
     --aug-prob 0.25 \
     --aug-types [translation,cutout,color] \
-    --image-size 128 \
+    --image-size 64 \
     --dual-contrast-loss \
     --use-aim \
     --aim_repo /home/allan/cppn-logs
