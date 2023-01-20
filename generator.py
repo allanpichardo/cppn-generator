69f871fe8efafaaf7ddd0d0d35f1c882073e9995
Allan Pichardo <allan.pichardo@gmail.com>
2023-01-19 21:44:21 -0800

    Added test of cppn generator

--- generator.py
+++ generator.py
@@ -1,6 +1,12 @@
 from lightweight_gan import Generator
+import torch
 
 
 if __name__ == '__main__':
     generator = Generator(image_size=128)
-    generator.cppn.generate_image((3, 128, 128), latent_dim=256,output_path="test_image.png", verbose=True)
+    generator.eval()
+
+    z = torch.randn(1, 256)
+    x = generator(z)
+
+    print(x.shape)
